0984b526a610baba80093efe9100be8f74460d99
melnik <nierfm@gmail.com>
2024-02-27 16:04:07 +0300

    changes

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
   private PostController controller;
+  private final String GET = "GET";
+  private final String POST = "POST";
+  private final String DELETE = "DELETE";
+  private final String API = "/api/posts";
 
   @Override
   public void init() {
@@ -25,21 +29,21 @@ public class MainServlet extends HttpServlet {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
       // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
+      if (method.equals(GET) && path.equals(API)) {
         controller.all(resp);
         return;
       }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+      if (method.equals(GET) && path.matches(API + "/\\d+")) {
         // easy way
         final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
         controller.getById(id, resp);
         return;
       }
-      if (method.equals("POST") && path.equals("/api/posts")) {
+      if (method.equals(POST) && path.equals(API)) {
         controller.save(req.getReader(), resp);
         return;
       }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+      if (method.equals(DELETE) && path.matches(API + "/\\d+")) {
         // easy way
         final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
         controller.removeById(id, resp);
